e3f84fd682f01d1e65ab7a87c01b7605b69e7b12
Lihuor <lihuor@gmail.com>
2020-07-31 21:55:53 +1000

    Add foreign key and delete some words

--- employee_Schema.sql
+++ employee_Schema.sql
@@ -3,25 +3,31 @@ CREATE database employee_DB;
 
 USE employee_DB;
 
+-- Creating DEPARTMENT TABLE
 CREATE TABLE deparment (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NULL,
-    song VARCHAR(100) NULL,
     PRIMARY KEY (id)
 );
 
+-- Creating ROLE Table
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NULL,
-    salary DECIMAL,
-    department_id INT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
     PRIMARY KEY (id)
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
+
+-- Creating EMPLOYEES TABLE
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NULL,
-    last_name VARCHAR(30) NULL,
-    role_id INT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
     manager_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
