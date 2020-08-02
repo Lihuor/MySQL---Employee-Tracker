5efd07197335294089fc6276268f8ae0298411a1
Lihuor <lihuor@gmail.com>
2020-08-02 21:20:51 +1000

    Fixed JS and installed NPMs

--- employeesData.sql
+++ employeesData.sql
@@ -1,49 +1,49 @@
 USE employees_DB;
 
------ Department -----
+----- Department Seeds -----
 
 INSERT INTO department (id, name)
-VALUES (1, "Sales");
+VALUES (1, "Retail");
 
 INSERT INTO department (id, name)
-VALUES (2, "Accounting");
+VALUES (2, "Brand Experience");
 
 INSERT INTO department (id, name)
-VALUES (3, "Warehouse");
+VALUES (3, "Human Resouces");
 
 INSERT INTO department (id, name)
-VALUES (4, "TeleMarketing");
+VALUES (4, "IT");
 
------ Role -----
-
-INSERT INTO role (id, title, salary, department_id)
-VALUES (1, "Door to Door Knock", 42000, 1);
+----- Role Seeds -----
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (2, "Telephone Sales", 60000, 1);
+VALUES (1, "Stylist", 42000, 1);
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (3, "Accounting Manager", 75000, 1);
+VALUES (2, "Assistant Store Manager", 60000, 1);
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (4, "Warehouse Manager", 65000, 2);
+VALUES (3, "Store Manager", 75000, 1);
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (5, "Warehouse Assistant", 70000, 3);
+VALUES (4, "Brand Manager", 65000, 2);
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (6, "TeleMarketing Manager", 100000, 3);
+VALUES (5, "Business Partner", 70000, 3);
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (7, "TeleMarketing Assistant", 80000, 4);
+VALUES (6, "HR Director", 100000, 3);
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (8, "TeleMarketing Agent", 52000, 4);
+VALUES (7, "System Adminstrator", 80000, 4);
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (9, "Managine Director", 105000, 4);
+VALUES (8, "IT Coordinator", 52000, 4);
 
------ Employees -----
+INSERT INTO role (id, title, salary, department_id)
+VALUES (9, "IT Manager", 105000, 4);
+
+----- Employees Seeds -----
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (4, "Charbelle", "Chaz", 3, null);
